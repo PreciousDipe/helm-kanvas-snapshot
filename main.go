363648bf4d9332551640744abc9696ad31f68326
Eti Ijeoma <53005190+Aijeyomah@users.noreply.github.com>
2025-06-19 11:42:54 +0100

    fix: create kanvas design

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	cmd "github.com/meshery-extensions/helm-kanvas-snapshot/cmd/kanvas-snapshot"
+	"github.com/layer5io/meshkit/logger"
 )
 
 var (
@@ -9,8 +10,10 @@ var (
 	mesheryCloudAPIBaseURL string
 	mesheryAPIBaseURL      string
 	workflowAccessToken    string
+	Log                    logger.Handler
 )
 
 func main() {
+	Log.Infof("email", providerToken)
 	cmd.Main(providerToken, mesheryCloudAPIBaseURL, mesheryAPIBaseURL, workflowAccessToken)
 }
